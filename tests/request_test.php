f95939355490ea52620ca6c58b9cd380c5d7e0d1
dariothornhill <dariothornhill@gmail.com>
2015-10-31 16:59:12 -0400

    Added 1st test

--- tests/request_test.php
+++ tests/request_test.php
@@ -1,5 +1,12 @@
 <?php
+require_once("../application.php");
+class requestTestCase extends PHPUnit_Testing_Framework{
 
-class requestTestCase extend PHPUnit_Testing_Framework{
-	
+	function test(){
+
+		$app = new Application();
+		$this->assertTrue(is_array($app->getArrayFromPost());
+
+	}
+
 }
