e7b794a0bcc56929ab3c4b61ba3abe5fc94b4044
bigkbear <kylesthill77@gmail.com>
2015-10-31 22:57:32 +0000

    Adding the front end code for application, index and new story

--- application.php
+++ application.php
@@ -1,14 +1,12 @@
 <?php
     class Application{
+        private $post= array();
+        
+        public function __construct($postarray){
+           $this->post = $postarray;
+        }
         function getArrayFromRequest(){
-            $thearray = array(
-                                'username'=>"steve",
-                                'post'=>"Today I learnt that you can only treat your friends so badly before they quit working for you and go and work for Halutte(sp)",
-                                2=>'String',
-                                3=>'Rand',
-                                4=>'Variable',
-                                5=>'" "');
-            return $thearray;
+            return $this->post;
         }
         
         function testGetArrayHasNameKey(){
--- index.php
+++ index.php
@@ -1,5 +1,12 @@
+<?php
+    require_once('application.php');
+    $app = new Application($_POST);
+?>
 <pre>
     <?php
         var_dump ($_POST);
+        var_dump($app);
     ?>
 </pre>
+Welcome <?php echo $app->getArrayFromRequest()["username"]; ?><br>
+You just added story:<?php echo $app->getArrayFromRequest()["post"]; ?>
