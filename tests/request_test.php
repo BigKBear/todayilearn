c525eaf26cd4b3e827311991f0838ab5dc16eea6
bigkbear <kylesthill77@gmail.com>
2015-10-31 21:27:04 +0000

    Ramake it

--- tests/request_test.php
+++ tests/request_test.php
@@ -1,11 +1,11 @@
 <?php
-require_once("../application.php");
-class requestTestCase extends PHPUnit_Testing_Framework{
+require_once("application.php");
+class requestTestCase extends PHPUnit_Framework_Testcase{
 
 	function test(){
 
 		$app = new Application();
-		$this->assertTrue(is_array($app->getArrayFromPost());
+		$this->assertTrue(is_array($app->getArrayFromPost()));
 
 	}
 
