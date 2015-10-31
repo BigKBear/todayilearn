40521130325e734672820b32d3c1692468135577
dariothornhill <dariothornhill@gmail.com>
2015-10-31 17:43:14 -0400

    Fixed the 2nd test cough

--- tests/request_test.php
+++ tests/request_test.php
@@ -1,11 +1,11 @@
 <?php
 require_once("application.php");
-class requestTestCase extends PHPUnit__Framework_Testcase{
+class requestTestCase extends PHPUnit_Framework_Testcase{
 
 	function testGetArrayFromRequest(){
 
 		$app = new Application();
-<<<<<<< HEAD
+
 		$this->assertTrue(is_array($app->getArrayFromRequest());
 
 	}
@@ -13,10 +13,9 @@ class requestTestCase extends PHPUnit__Framework_Testcase{
 	function testGetArrayHasNameKey(){
 
 		$app = new Application();
-		$this->assertTrue(array_key_exists(key,($app->getArrayFromRequest());
-=======
-		$this->assertTrue(is_array($app->getArrayFromPost()));
->>>>>>> 03e1ceb025448f33960a0498db6a7ac7ed829c04
+		$array = $app->getArrayFromRequest();
+		$this->assertTrue(array_key_exists('username',$array);
+
 
 	}
 
