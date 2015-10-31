239367a1a5472706829b21352d5c938648141522
bigkbear <kylesthill77@gmail.com>
2015-10-31 22:41:21 +0000

    new story linked to the index

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
-<?php
-    echo'Hello';
-?>
+<pre>
+    <?php
+        var_dump ($_POST);
+    ?>
+</pre>
---
+++ new_story.php
@@ -0,0 +1,9 @@
+<?php
+    
+?>
+<form action="index.php" method="post">
+    <input type="text" name="username" placeholder="Username..."><br><br>
+    <textarea rows="4" cols="50" name="post" placeholder="Tell us what you learned..."></textarea>
+    <br>
+    <input type="submit">
+</form>
