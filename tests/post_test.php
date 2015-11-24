0548359489228dd5c6cb340299767f63962a77dc
aceakh <aceakh@gmail.com>
2015-11-24 19:44:44 +0000

    Now using a better implementation of the factory pattern than what we started with

---
+++ tests/post_test.php
@@ -0,0 +1,8 @@
+<?php 
+// /tests/post_test.php
+
+require_once __DIR__.'/bootstrap.php';
+class PostTest extends PHPUnit_Framework_TestCase
+{
+    // moved the test to /tests/TIL/Tests/post_test.php
+}
